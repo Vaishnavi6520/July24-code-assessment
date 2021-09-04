cb2e016f4eb8d338e210cab5177e2d0e700c14d7
Vaishnavi Hajela <65701834+Vaishnavi6520@users.noreply.github.com>
2021-09-04 15:12:35 +0530

    4sept

---
+++ 04sept2021-Code-Assessment7/04sept2021-Vaishnavi-CodeAssessment/CollegeProject/faculty/urls.py
@@ -0,0 +1,29 @@
+from . import views 
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    
+    path('add/',views.faculty,name='student'),
+    path('viewall/',views.faculty_list,name='student_list'),
+    path('view/<id>',views.facultydetail,name='studentdetail'),
+    path('searchh/',views.searchp,name='search'),
+    path('updatee/',views.updatee,name='update'),
+    path('apiupdatee/',views.apiupdatee,name='apiupdate'),
+    path('deletee/',views.deletee,name='delete'),
+    path('apideletee/',views.apideletee,name='apidelete'),
+
+
+    
+    path('',views.addpagee,name='addpage'),
+    path('viewpage1/',views.viewpagee,name='viewpage'),
+    path('searchpage1/',views.searchpagee,name='searchpage'),
+    path('updatepage1/',views.updatepagee,name='updatepage'),
+    path('deletepage1/',views.deletepagee,name='deletepage'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('viewprofile/', views.profile, name='profile'),
+    path('log/',views.logindetail,name='logindetail')
+    
+    
+]
